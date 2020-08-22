a6421bb435fcc09349d7b5b9ff5a4e06c9732a97
Oscar Bataille <oscar.bataille@vessinge.se>
2020-08-22 16:54:47 +0200

    first commit

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<?php
+
+ini_set('max_execution_time', 0);
+
+// Autoload
+require __DIR__ . '/vendor/autoload.php';
+
+
---
+++ test2.sh
@@ -0,0 +1,4 @@
+curl  -v  \
+-H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+-d  "@postdata.txt"   \
+'https://www.umu.se/utbildning/api/Schema/Index'
