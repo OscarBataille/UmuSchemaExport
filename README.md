489c103a29252b1f84250b8633e28f8357b6c326
Oscar Bataille <oscar.bataille@vessinge.se>
2020-08-23 16:01:06 +0200

    add image and remove unused files

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Scrape Umeå university calendar and export it as an iCalendar.
 
+
+![Image](/static/Schedule.png)
+
 ```php index.php > ical.ics```
--- test2.sh
+++
@@ -1,4 +0,0 @@
-curl  -v  \
--H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
--d  "@postdata.txt"   \
-'https://www.umu.se/utbildning/api/Schema/Index'
