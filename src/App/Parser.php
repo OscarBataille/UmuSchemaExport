b06a8f422b676f46a1a56f49f1d536d8dfb35aef
Oscar Bataille <oscar.bataille@vessinge.se>
2020-08-23 11:53:28 +0200

    working prototype

---
+++ src/App/Parser.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App;
+
+class Parser
+{
+
+    public static function parse(string $htmlResponse)
+    {
+        $htmlDoc = new \DOMDocument();
+        $htmlDoc->loadHtml($htmlResponse);
+
+        
+        $xpath = new \DOMXpath($htmlDoc);
+
+        $elements = $xpath->query("//div[contains(@class, 'veckodagar')]");
+
+        $calendarEvents = [];
+
+
+        foreach ($elements as $key => $element) {
+            $date = $element->getAttribute('id');
+
+            // Get the events
+            foreach ($xpath->query(".//div[contains(@class, 'schemalista')]", $element) as $eventData) {
+
+                // echo $htmlDoc->saveHTML($eventData);
+
+                $calendarEvent = ["date" => $date];
+
+                // If no events, save the day and stop the iteration
+                if (trim($eventData->nodeValue) == '') {
+                    $calendarEvents[$date] = $calendarEvent;
+                    continue;
+                }
+
+                // Extract title
+                $titles = $xpath->query(".//h3", $eventData);
+
+                $calendarEvent["title"] = $titles->item(0)->nodeValue;
+
+                // Extract Start time
+                $spanStartTime = $xpath->query(".//span[contains(@class, 'starttid')]", $eventData);
+
+                $calendarEvent["startTime"] = $spanStartTime->item(0)->nodeValue;
+
+                // Extract stop time
+                $spanStopTime = $xpath->query(".//span[contains(@class, 'stopptid')]", $eventData);
+
+                $calendarEvent["stopTime"] = $spanStopTime->item(0)->nodeValue;
+
+                // Extract event time
+                $detail = $xpath->query(".//span[@class='eventtyp']/span[contains(@class, 'detaljer')]", $eventData);
+
+                $calendarEvent["eventType"] = trim($detail->item(0)->nodeValue);
+
+                // Extract location time
+                $location = $xpath->query(".//span[@class='lokal']/span[contains(@class, 'detaljer')]/a", $eventData);
+
+                $calendarEvent["location"] = trim($location->item(0)->nodeValue);
+
+                // Extract event info
+                foreach ($xpath->query(".//div[@class='visamerinfo']", $eventData) as $moreInfo) {
+
+                    $spanMoreInfo = $xpath->query(".//span", $moreInfo);
+
+                    $calendarEvent["moreInfo"][] = trim($spanMoreInfo->item(0)->nodeValue) . " " . trim($spanMoreInfo->item(1)->nodeValue);
+                }
+
+                // Add the event to the calendar
+                $calendarEvents[$date] = $calendarEvent;
+
+            }
+
+        }
+
+        return $calendarEvents;
+    }
+}
--- src/index.php
+++ src/index.php
@@ -9,79 +9,43 @@ use GuzzleHttp\Psr7\Request;
 
 $client = new GuzzleHttp\Client();
 
-$response = $client->request('POST', "https://www.umu.se/utbildning/api/Schema/Index", [
-
-    'form_params' => [
-        "btn"                            => "v+",
-        "Kurskoder"                      => "2KG052",
-        "Instanskoder"                   => "29030HT20",
-        "Year"                           => "2020",
-        "Month"                          => "9",
-        "Day"                            => "3",
-        "Lang"                           => "en",
-        "ValdDag"                        => "-1",
-        "PreviewPage"                    => "",
-        "Manadsvy"                       => "False",
-        "Kurser[0].Namn"                 => "Sveriges sociala geografi, 7.5 hp",
-        "Kurser[0].Instanskod"           => "29030HT20",
-        "Kurser[0].FargClass"            => "farg_0",
-        "Kurser[0].AlternativSchemalank" => "",
-        "Kurser[0].HarPubliceratSchema"  => "True",
-        "Kurser[0].AntalValdaGrupper"    => "0",
-        "Kurser[0].Vald"                 => "true",
-        "X-Requested-With"               => "XMLHttpRequest",
-    ],
-]);
-
-$body = (string) $response->getBody();
-
-$htmlDoc = new DOMDocument();
-$htmlDoc->loadHtml($body);
-
-// echo $htmlDoc->saveHTML();
-$xpath = new DOMXpath($htmlDoc);
-
-$elements = $xpath->query("//div[contains(@class, 'veckodagar')]");
-
+$currentDate = strtotime("last Monday - 1 day");
 $calendarEvents = [];
+$endDate = strtotime("2020-12-25");
 
-//Loop through days element
-foreach ($elements as $key => $element) {
-    $date = $element->getAttribute('id');
+while ($currentDate < $endDate) {
+    $dayPreviousWeek   = (string) date("d", $currentDate);
+    $monthPreviousWeek = (string) date("m", $currentDate);
+    $yearPreviousWeek  = (string) date("Y", $currentDate);
 
-    // Get the events
-    foreach ($xpath->query("div[contains(@class, 'schemalista')]", $element) as $eventData) {
-        $calendarEvent = ["date" => $date];
+    $response = $client->request('POST', "https://www.umu.se/utbildning/api/Schema/Index", [
 
-        // If no events, save the day and stop the iteration
-        if (trim($eventData->nodeValue) == '') {
-            $calendarEvents[] = $calendarEvent;
-            continue;
-        }
+        'form_params' => [
+            "btn"                            => "v+",
+            "Kurskoder"                      => "2KG052",
+            "Instanskoder"                   => "29030HT20",
+            "Year"                           => $yearPreviousWeek,
+            "Month"                          => $monthPreviousWeek,
+            "Day"                            => $dayPreviousWeek,
+            "Lang"                           => "en",
+            "ValdDag"                        => "-1",
+            "PreviewPage"                    => "",
+            "Manadsvy"                       => "False",
+            "Kurser[0].Namn"                 => "Sveriges sociala geografi, 7.5 hp",
+            "Kurser[0].Instanskod"           => "29030HT20",
+            "Kurser[0].FargClass"            => "farg_0",
+            "Kurser[0].AlternativSchemalank" => "",
+            "Kurser[0].HarPubliceratSchema"  => "True",
+            "Kurser[0].AntalValdaGrupper"    => "0",
+            "Kurser[0].Vald"                 => "true",
+            "X-Requested-With"               => "XMLHttpRequest",
+        ],
+    ]);
 
-        // Extract title
-        $titles = $xpath->query("//h3", $eventData);
+    $body = (string) $response->getBody();
 
-        $calendarEvent["title"] = $titles->item(0)->nodeValue;
+    $calendarEvents = array_merge($calendarEvents, App\Parser::parse($body));
 
-        // Extract Start time
-        $spanStartTime = $xpath->query("//span[contains(@class, 'starttid')]", $eventData);
-
-        $calendarEvent["startTime"] = $spanStartTime->item(0)->nodeValue;
-
-        // Extract stop time
-        $spanStopTime = $xpath->query("//span[contains(@class, 'stopptid')]", $eventData);
-
-        $calendarEvent["stopTime"] = $spanStopTime->item(0)->nodeValue;
-
-
-        echo $htmlDoc->saveHTML($eventData);
-
-        // Add the event to the calendar
-        $calendarEvents[] = $calendarEvent;
-
-    }
-
+    $currentDate = strtotime("+1 week", $currentDate);
 }
-
 var_dump($calendarEvents);
