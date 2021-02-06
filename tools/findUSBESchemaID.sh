73f3987c1d6d556e550230882c713cc74e98988d
Oscar Bataille <oscar.bataille@vessinge.se>
2021-02-06 16:07:16 +0100

    Add tool to find USBE schema ID for a course code

---
+++ tools/findUSBESchemaID.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "No Course code provided"
+    exit 1
+fi
+
+
+for i in {2300..4000}
+do
+ if curl -f -s  http://www.hh.umu.se/usbeweb/fek/schema/schema.php?ID=$i | grep -q $1
+ then echo $i
+fi
+done
+
